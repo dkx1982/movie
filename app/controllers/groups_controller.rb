c7201a571dbd1cabfa95ec24ff720ea09bc5deff
“dkx1982” <dkx1982@163.com>
2017-03-05 14:04:14 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  validates :title, presence: true
   def index
     @groups = Group.all
   end
@@ -23,9 +24,11 @@ class GroupsController < ApplicationController
   end
   def create
     @group =Group.new(group_params)
-    @group.save
-
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def destroy
