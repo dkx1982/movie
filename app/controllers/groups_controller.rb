0a42be9e4953ef225be7a1bccc7c1e80fe445306
“dkx1982” <dkx1982@163.com>
2017-03-17 14:36:45 +0800

    done find group id and check permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
 
-  before_action :authenticate_user! ,only: [:new, :create ]
+  before_action :authenticate_user! ,only: [:new, :create, :edit, :update, :destroy ]
+  before_action :find_id_and_check_permission ,only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -8,15 +9,16 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+
   end
 
   def edit
-    @group = Group.find(params[:id])
-
+    find_id_and_check_permission
   end
 
   def update
-    @group = Group.find(params[:id])
+    find_id_and_check_permission
+
     if @group.update(group_params)
       redirect_to groups_path, notice:"update Success"
     else
@@ -39,13 +41,21 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+    find_id_and_check_permission
     @group.destroy
     flash[:alert]="Group deleted"
     redirect_to groups_path
   end
 
   private
+
+  def find_id_and_check_permission
+    @group = Group.find(params[:id])
+    if @group.user != current_user
+      redirect_to root_path, alert: "不好意思，你不是创建者不能操作。"
+    end
+  end
+
   def group_params
     params.require(:group).permit(:title, :description)
   end
