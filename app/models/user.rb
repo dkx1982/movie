12d01530d1744e38c7755a9f325a9e3b5444e2a7
“dkx1982” <dkx1982@163.com>
2017-03-19 11:01:55 +0800

    is_member_of?

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
 
   has_many :groups
   has_many :posts
+  has_many :group_relationships
+  has_many :partpost_groups, :through => :group_relationships, :source => :group
+
+  def is_member_of?(group)
+    partpost_groups.include?(group)
+  end
 end
